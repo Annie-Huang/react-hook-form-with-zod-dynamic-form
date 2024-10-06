17da641e6a1dfbca96592b2039eac44fb7f6bc6d
Annie Huang <annie.hn@gmail.com>
2024-10-06 18:01:47 +1100

    Add the Material ThemeProvider into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
+// https://mui.com/material-ui/api/css-baseline/
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          mode: 'dark',
+        },
+      })}
+    >
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
