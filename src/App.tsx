b87a30d1d2823d2fc9f838996b60d0b34d6e707f
Annie Huang <annie.hn@gmail.com>
2024-10-06 18:36:05 +1100

    Use useForm in App.tsx and add register, helperText and error into the TextField. Click into the field and click outside and we can see the error message.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Container } from './Container.tsx';
 import { TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { formDefaultValues, formSchema, FormSchema } from './formSchema.ts';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export const App = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    mode: 'all',
+    resolver: zodResolver(formSchema),
+    defaultValues: formDefaultValues,
+  });
+
   return (
     <Container>
-      <TextField label='Full Name' />
+      <TextField
+        {...register('fullName')}
+        label='Full Name'
+        helperText={errors.fullName?.message}
+        error={!!errors.fullName}
+      />
     </Container>
   );
 };
