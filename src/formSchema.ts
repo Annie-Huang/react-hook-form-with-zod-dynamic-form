dab2005edffc18dc4500478be09cdc1b1aab0552
Annie Huang <annie.hn@gmail.com>
2024-10-10 17:27:58 +1100

    Explain discriminatedUnion

--- src/formSchema.ts
+++ src/formSchema.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod';
 
+/*
+https://zod.dev/?id=discriminated-unions
+Zod can check the discriminator key (status in the example above) to determine which schema should be used to parse the input. This makes parsing more efficient and lets Zod report friendlier errors.
+*/
+const workExperienceSchema = z.discriminatedUnion();
+
 const formSchema = z.object({
   fullName: z.string().min(1),
 });
